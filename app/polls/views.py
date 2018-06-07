e73a58547435a6537aa03dddaa3ff97c02217f24
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-07 22:02:00 +0900

    index view에서 동적으로 Question 목록을 보여줌

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("hello world. You're at the polls index")
+    # DB에 있는 Question중, 가장 최근에 발행(pub_date)된 순서대로 최대 5개에 해당하는 QuerySet을
+    # latest_question_list 변수에 할당
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # latest_question_list의 각 Question의 qusetion_text들을 ', '로 연결시킨 문자열을
+    # output 변수에 할당
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    # 만들어진 질문 제목들을 모은 문자열을 HttpResponse 클래스의 생성자로 전달, 인스턴스를 리턴
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
